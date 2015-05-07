71d1bfe091694e8948d0cdde3987c185b6096870
Richard Johnson <rjohnson2590@gmail.com>
2015-05-07 11:06:53 -0700

    This works?

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-make a change
+function sayHello(){
+	console.log('Hello World!');
+}
+
+function superSecret() {
+	consloe.log('Noooooooo you cant do that');
+}
+
+module.exports = {
+	version: '0.0.1',
+	otherName: sayHello
+};
